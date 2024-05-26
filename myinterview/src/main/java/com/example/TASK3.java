ab50a1e3c847276638b6bfa68f4727c4ec1c8a2f
Lucas Viegas <lucas.viegas3005@gmail.com>
2024-05-26 15:53:28 -0300

    codigo task3

--- myinterview/src/main/java/com/example/TASK3.java
+++ myinterview/src/main/java/com/example/TASK3.java
@@ -2,6 +2,7 @@ package com.example;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.Scanner;
 
 /**
  * Write a list and add an aleatory number of Strings. In the end, print out how
@@ -10,5 +11,36 @@ import java.util.Iterator;
  */
 public class TASK3 {
 
+    public static String itensDistintos() {
+        int distintos = 0;
+        String proximo = "s";
+
+        Scanner scan = new Scanner(System.in);
+
+        ArrayList<String> strings = new ArrayList<String>();
+
+        while (proximo.equals("s")) {
+            System.out.println("Digite uma string: ");
+            String stringEntrada = scan.nextLine();
+
+            if (!strings.contains(stringEntrada)) {
+                distintos++;
+            }
+            strings.add(stringEntrada);
+
+            System.out.println("Deseja adicionar mais strings? s/n ");
+            proximo = scan.nextLine();
+        }
+
+        scan.close();
+
+        System.out.println(strings);
+        return "A lista contém " + distintos + " itens distintos";
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(itensDistintos());
+    }
   
 }
